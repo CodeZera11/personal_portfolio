6fd1ae2780904e28291a13224d37de7380eaeb9e
Bhavesh Yadav <codezera3@gmail.com>
2023-07-28 00:06:04 +0530

    added slash in skill card

--- components/SkillCard.jsx
+++ components/SkillCard.jsx
@@ -7,10 +7,11 @@ const SkillCard = ({ title, img }) => {
       <div className="flex items-center justify-between p-5 border rounded-xl shadow-xl shadow-gray-800 hover:scale-105 ease-in duration-200 border-black bg-gray-600">
         <div>
           <Image
-            src={`/../public/assets/skills/${img}.png`}
+            // src={`/../public/assets/skills/${img}.png`}
+            src={`/public/assets/skills${img}.png`}
             width={64}
             height={64}
-            alt="html"
+            alt={img}
           />
         </div>
         <h1 className="text-2xl">{title}</h1>
